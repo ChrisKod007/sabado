886bb268b0f566f8dc7c5f05603d022916223b05
ChrisKod007 <cdbm001@gmail.com>
2024-09-14 17:10:21 -0500

    Update test.py
    Bot hiperpotenciado en crecimiento

--- test.py
+++ test.py
@@ -1,4 +1,26 @@
-print("Hola chicos, este es el primer repositorio de ejemplo de mi perfil...")
-print("algo nuevo")
-# Esto es para comprobar versiones
-print("algo nuevo")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='*', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Conexión exitosa, iniciamos sesión como: {bot.user}')
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def risa(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("TOKEN")
